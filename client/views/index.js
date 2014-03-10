438adf42d719dde39a571923d866cf7167d7b5fd
Theodor Vararu <theo@tvararu.ro>
2014-03-10 17:46:17 +0200

    Still trying to fix the background image issue

--- client/views/index.js
+++ client/views/index.js
@@ -1,5 +1,7 @@
 UI.body.rendered = function() {
-  $('.title-block').addClass('animate');
+  $(window).load(function() {
+    $('.title-block').addClass('animate');
+  });
 };
 
 // UI.body.rendered = function() {
